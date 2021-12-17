21d283437176836402f3e43c15ed0ba302396b1b
Hersh Nagpal <hershnagpal@gmail.com>
2021-12-17 02:37:24 -0500

    Yee

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react"
+import { BrowserRouter, Link, Route, Routes, } from 'react-router-dom';
 import Grid from "./components/grid/Grid"
 
 const App = () => {
     return  (
         <div>
-            <Grid />
-            <p>yee</p>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Grid />} />
+                    <Route path="yee" element={<Link to="/">yee</Link>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
-}
+};
 
-export default App
+export default App;
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,11 +1,13 @@
 import { AppBar, Typography } from '@material-ui/core';
-
+import { FixedSizeLink, Link } from 'react-router-dom';
 
 const Grid = () => {
     return (
         <AppBar position="static" color="inherit">
             <Typography variant="h2" align="center">MERN Test</Typography>
+            <Link to="/yee"> Go To Yee </Link>
         </AppBar>
+
     )
 };
 
