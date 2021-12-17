e01d3f3a19a9090a095909dcf413316f3723a346
Hersh Nagpal <hershnagpal@gmail.com>
2021-12-17 02:21:30 -0500

    MERN Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Grid from "./components/grid/Grid"
 
 const App = () => {
     return  (
         <div>
-            yeet
+            <Grid />
+            <p>yee</p>
         </div>
     )
 }
---
+++ src/components/grid/Grid.js
@@ -0,0 +1,12 @@
+import { AppBar, Typography } from '@material-ui/core';
+
+
+const Grid = () => {
+    return (
+        <AppBar position="static" color="inherit">
+            <Typography variant="h2" align="center">MERN Test</Typography>
+        </AppBar>
+    )
+};
+
+export default Grid;
