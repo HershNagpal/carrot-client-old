4e20248649a118de381107d616552168823558f4
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2021-12-20 15:46:40 -0500

    Scaled font size on grid

--- src/components/gameGrid/tile/styles.js
+++ src/components/gameGrid/tile/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
     tile: {
         background: 'lightgreen',
-        fontSize: '72px',
+        fontSize: '5vh',
         justifyContent: 'center',
         color: 'black',
         height: '5vh',
