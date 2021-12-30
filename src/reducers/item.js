42d4f9c5ed852259ca29009415b183f0c99359ec
Hersh Nagpal <hershnagpal@gmail.com>
2021-12-30 00:19:24 -0500

    Vowed Mithril Spell-carrot works

--- src/reducers/item.js
+++ src/reducers/item.js
@@ -1,13 +1,16 @@
 import * as constants from '../constants';
+import { getPlayerCoords } from './selectors';
+import { setTile, setTileEntity } from './movement';
+import { checkMove } from './moveHelpers';
 
 export const doUseSuperCarrot = (game) => {
     switch (game.inventorySuperCarrot) {
         case 0:
             console.log('Carrot of Riddles');
-            return game;
+            return carrotOfRiddles(game);
         case 1:
             console.log('Vowed Mithril Spell-carrot');
-            return game;
+            return randomPlayerTeleport(game);
         case 2:
             console.log('Relentless Steel Carrot');
             return game;
@@ -25,6 +28,37 @@ export const doUseSuperCarrot = (game) => {
 
 export const doUnequipSuperCarrot = (game) => (
     {...game, inventorySuperCarrot: -1 }
-)
+);
 
+// Vowed Mithril Spell-carrot
+const randomPlayerTeleport = (game) => {
+    const { x, y } = getPlayerCoords(game.grid);
+
+    const coords = randomGrassLocation(game);
+    console.log(coords);
+    
+    const spawnPlayer = (game) => (
+        setTileEntity({ x: coords.x, y: coords.y}, game.grid[y][x], game)
+    );
+    const removePlayer = (game) => (
+        setTile({ x: x, y: y}, 'grass', game)
+    );
+
+    const stateChanges = [ spawnPlayer, removePlayer]
+    return stateChanges.reduce((a, stateChange) => (
+        stateChange(a)
+    ), game);
+};
+
+const randomGrassLocation = (game) => {
+    while (true) {
+        const x = Math.floor(Math.random() * 15);
+        const y = Math.floor(Math.random() * 15);
+        if (game.grid[y][x].entity.type === 'grass') {
+            return { x: x, y: y };
+        }
+    }
+};
+
+// Carrot of Riddles lol
 const carrotOfRiddles = (game) => game;
