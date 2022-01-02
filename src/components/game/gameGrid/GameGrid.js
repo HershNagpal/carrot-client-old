484402167d07cd33f0725ab8aca5a973de63d052
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2022-01-01 21:07:18 -0500

    added default case

--- src/components/game/gameGrid/GameGrid.js
+++ src/components/game/gameGrid/GameGrid.js
@@ -31,6 +31,8 @@ const GameGrid = () => {
                 case 'v':
                     dispatch(swapPocket());
                     break;
+                default:
+                    break;
             }
         } else {
             switch (key) {
