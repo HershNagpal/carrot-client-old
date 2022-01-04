5c500f4581188195f5ec5644ca823c997cf0448a
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2022-01-04 05:10:05 -0500

    can view collection from game over screen

--- src/components/game/gameOver/GameOver.js
+++ src/components/game/gameOver/GameOver.js
@@ -1,14 +1,20 @@
 import useStyles from './styles';
 import * as constants from '../../../constants';
 import { carrotIcon, pocketIcon, itemIcons } from '../../../images';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container, Typography, Button, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { toggleCollection } from '../../../actions/game';
 
 const GameOver = () => {
     const game = useSelector((state) => state.game);
     const classes = useStyles();
+    const dispatch = useDispatch();
     
+    const doToggleCollection = () => (
+        dispatch(toggleCollection())
+    );
+
     return <>
         <Container className={classes.outerContainer}>
             <p className={classes.gameOverText}>get rekt scrub</p>
@@ -48,6 +54,7 @@ const GameOver = () => {
             <Link to="/">
                 <Button className={classes.titleButton} variant="contained" color="primary">Title Screen</Button>
             </Link>
+            <Button variant="contained" color="primary" onClick={doToggleCollection}>Collection</Button>
         </Container>
         
         <div className={classes.gameOverBackground} />
